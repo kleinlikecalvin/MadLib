bf85e6980865e372d94009c0aa1d4c20c4797144
streetlightkids <ltj_hq@hotmail.com>
2021-06-22 10:15:17 -0400

    attempt 2 at testing input value
    Attempt 2 failed. I can't seem to figure out the right way to test the value

---
+++ main.js
@@ -0,0 +1,26 @@
+//Get the button so I can add an event listener
+var makeStoryButton = document.getElementById('story-maker-button');
+makeStoryButton.addEventListener('click', testUserInput);
+
+//Get the user input value so I can test that it's been entered and so I can use it in the story.
+function testUserInput(){
+    var userInput = document.getElementsByName("input");  
+    if(userInput.value == null)
+    {
+        alert("You missed a spot.");
+    }
+    makeStoryFunction();
+}
+
+
+//Create a function to generate the story
+var nounInputValue = document.getElementById('noun-input');
+var adverbInputValue = document.getElementById('adverb-input');
+var nameInputValue = document.getElementById('name-input');
+function makeStoryFunction()
+{
+    var storyTitle = document.getElementById('user-story-title');
+    var storyContents = document.getElementById('user-story-contents');
+    storyTitle.innerHTML = "I think I'm gonna be sick";
+    storyContents.innerHTML = "Earlier I was " + adverbInputValue.value + " minding my own damn business when all of the sudden " + nameInputValue.value + " appeared out of no where and hit me with a " + nounInputValue.value + ". They gave me a concussion and I've been throwing up nonstop since. Man, I can't stand " + nameInputValue.value + "!";
+};
