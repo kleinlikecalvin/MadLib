dbeb4011db651e6e10fb5dbd6a657b2f1ad1f9eb
streetlightkids <ltj_hq@hotmail.com>
2021-06-25 11:44:09 -0400

    deleted old js file
    Still figuring out how to manage GitHub apparently

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-        //Get the input fields so that I can test if they've been filled out
-        var inputField = document.getElementsByName('input');
-        if (inputField.value <= 0) {
-            var inputError = alert("You missed a spot.");
-        }
-        else
-        {
-            makeStoryFunction;
-        }
-        //Get the button so I can add an event listener
-        var makeStoryButton = document.getElementById('story-maker-button');
-        //Create a function to generate the story
-        var makeStoryFunction = function()
-        {
-                var storyTitle = document.getElementById('user-story-title');
-                var nounInputValue = document.getElementById('noun-input');
-                var adverbInputValue = document.getElementById('adverb-input');
-                var nameInputValue = document.getElementById('name-input');
-                var storyContents = document.getElementById('user-story-contents');
-                storyTitle.innerHTML = "I think I'm gonna be sick";
-                storyContents.innerHTML = "Earlier I was " + adverbInputValue.value + " minding my own damn business when all of the sudden " + nameInputValue.value + " appeared out of no where and hit me with a " + nounInputValue.value + ". They gave me a concussion and I've been throwing up nonstop since. Man, I can't stand " + nameInputValue.value + "!";
-        };
-        makeStoryButton.addEventListener('click', makeStoryFunction);
+//Get the button so I can add an event listener
+var makeStoryButton = document.getElementById('story-maker-button');
+makeStoryButton.addEventListener('click', testUserInput);
+
+const form = document.forms["user-input-form"];
+form.addEventListener('submit', event => event.preventDefault());
+
+//Get the user input value so I can test that it's been entered and so I can use it in the story.
+function testUserInput() {
+    if (form.checkValidity()) {
+        makeStoryFunction();
+    }
+}
+
+
+//Create a function to generate the story
+var nounInputValue = document.getElementById('noun-input');
+var adverbInputValue = document.getElementById('adverb-input');
+var nameInputValue = document.getElementById('name-input');
+
+function makeStoryFunction() {
+    var storyTitle = document.getElementById('user-story-title');
+    var storyContents = document.getElementById('user-story-contents');
+    storyTitle.innerText = "I think I'm gonna be sick";
+    storyContents.innerText = "Earlier I was " + adverbInputValue.value + " minding my own damn business when all of the sudden " + nameInputValue.value + " appeared out of no where and hit me with a " + nounInputValue.value + ". They gave me a concussion and I've been throwing up nonstop since. Man, I can't stand " + nameInputValue.value + "!";
+};
